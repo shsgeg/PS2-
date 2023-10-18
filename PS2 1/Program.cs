0d44e68d79039241c5140cc5369f0c736d731c2e
sasha <sasha@DESKTOP-ICAV6SC>
2023-10-18 16:39:10 +0300

    Добавьте файлы проекта.

---
+++ PS2 1/Program.cs
@@ -0,0 +1,31 @@
+﻿int Factorial(int nums)
+{
+    int res = 1;
+    for (int i = nums; i > 1; i--)
+        res *= i;
+    return res;
+}
+
+void SumCalculate(double e, out double sum, int n, out int count) 
+{ 
+    sum = 0;
+    count = 0;
+    int k = 0;
+    double a = 1;
+
+    while (Math.Abs(a) > e)
+    {
+        a = Math.Pow(a, 2 * k) / Factorial(2 * k);
+        sum += a;
+        k++;
+        count++;
+    }
+
+}
+
+double s;
+int c;
+SumCalculate(0.00001, out s, 1000, out c);
+Console.WriteLine($"Сумма ряда с заданной точностью epsilon: {Math.Cos(s)}");
+Console.WriteLine($"Точность достигается на шаге: {c}");
+
